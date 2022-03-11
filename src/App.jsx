4f48a3d92e9a0bd221d285e55064fa5a53143ac3
Sadanand Pai <sadanand.pai@coindcx.com>
2022-03-11 09:51:15 +0530

    router animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const routes = ["/shuffler-1D", "/shuffler-2D", "/stagger"];
 
 function App() {
   const location = useLocation();
@@ -6,15 +9,18 @@ function App() {
   return (
     <>
       <nav className="w-11/12 m-auto text-center mb-4">
-        <Link to="/shuffler-1D" className="p-2 inline-flex bg-black text-white m-2">
-          Shuffler 1D
-        </Link>
-        <Link to="/shuffler-2D" className="p-2 inline-flex bg-black text-white m-2">
-          Shuffler 2D
-        </Link>
-        <Link to="/stagger" className="p-2 inline-flex bg-black text-white m-2">
-          Stagger
-        </Link>
+        {routes.map((route) =>
+          location.pathname === route ? (
+            <Link key={route} to={route} className="p-2 inline-flex  text-blue-700 m-2 relative">
+              {route.slice(1)}
+              <motion.div className="absolute border-b-2 border-green-700 w-10/12 mt-6" layoutId="highlight"></motion.div>
+            </Link>
+          ) : (
+            <Link key={route} to={route} className="p-2 inline-flex  text-blue-700 m-2">
+              {route.slice(1)}
+            </Link>
+          )
+        )}
       </nav>
       <Outlet />
     </>
